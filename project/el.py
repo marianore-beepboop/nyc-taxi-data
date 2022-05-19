05afed3695dda483d4732025fbfc31565a107590
marianore-beepboop <marianoreinfo@gmail.com>
2022-05-18 23:09:40 -0300

    Add new docker compose files and its dependencies

--- project/el.py
+++ project/el.py
@@ -5,18 +5,21 @@ from project.constants import DATA_DIR
 from project.helpers import get_db_uri
 from sqlalchemy import create_engine
 
+def main():
+    file_name = "yellow_tripdata_2021-01_100k_head"
 
-file_name = "yellow_tripdata_2021-01"
+    db_uri = get_db_uri(PG_CFG)
+    engine = create_engine(db_uri)
 
-db_uri = get_db_uri(PG_CFG)
-engine = create_engine(db_uri)
+    with open(f"{DATA_DIR}/{file_name}.csv") as f:
+        df = pd.read_csv(f, sep=",", nrows=100)
 
-with open(f"{DATA_DIR}/{file_name}.csv") as f:
-    df = pd.read_csv(f, sep=",", nrows=100)
+    df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
+    df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
 
-df.tpep_pickup_datetime = pd.to_datetime(df.tpep_pickup_datetime)
-df.tpep_dropoff_datetime = pd.to_datetime(df.tpep_dropoff_datetime)
+    pd.io.sql.get_schema(df, name="yellow_taxi_data", con=engine)
 
-pd.io.sql.get_schema(df, name="yellow_taxi_data", con=engine)
+    df.to_sql(name="yellow_taxi_data", con=engine, if_exists="replace")
 
-df.to_sql(name="yellow_taxi_data", con=engine, if_exists="replace")
+if __name__ == "__main__":
+    main()
